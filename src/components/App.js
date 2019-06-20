31f36f5f3126846964037ad000958c581f0fa3ff
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-20 17:38:16 +0200

    add onInputChange function

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,20 @@ import Header from './Header';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = {
+    inputValue: '',
+  };
+
+  onInputChange = (e) => {
+    this.setState({inputValue: e.target.value});
+    // console.log(this.state.inputValue);
+  };
 
   render() {
     return(
       <div>
         <Header />
-        <SearchBar />
+        <SearchBar onInputChange={this.onInputChange}/>
       </div>
     );
   };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ const SearchBar = (props) => {
 
     return(
         <form className="container">
-            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+            <input onChange={props.onInputChange} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
             <button type="submit" className="btn btn-primary btn-lg btn-block">Search new recipes</button>
         </form>
     );
