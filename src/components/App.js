69abaf4172a08d01e332b9431859b302f04ccf5d
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-20 18:53:03 +0200

    add onFormSubmit function

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
   state = {
     inputValue: '',
+    recipes: []
   };
 
   onInputChange = (e) => {
@@ -12,11 +13,40 @@ class App extends React.Component {
     // console.log(this.state.inputValue);
   };
 
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submit');
+    const app_id = 'b8f9837b';
+    const app_key = '93b80432c253167006389245910e5a22';
+    const apiUrl = `https://api.edamam.com/search?q=${this.state.inputValue}&app_id=${app_id}&app_key=${app_key}`;
+    
+    fetch(apiUrl)
+      .then(response => {
+        if(response.ok) {
+          return response;
+        }
+        throw Error('Something went wrong, try again')
+      })
+      .then(response => response.json())
+      .then(result => {
+        this.setState({
+          recipes: [...result.hits]
+        });
+      })
+      .catch(err => {
+        // this.setState({error: true})
+        console.log(err)
+      })
+      console.log(this.state.recipes)
+  };
+
   render() {
     return(
       <div>
         <Header />
-        <SearchBar onInputChange={this.onInputChange}/>
+        <SearchBar 
+        onInputChange={this.onInputChange}
+        onFormSubmit={this.onFormSubmit}/>
       </div>
     );
   };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBar = (props) => {
 
     return(
-        <form className="container">
+        <form onSubmit={props.onFormSubmit} className="container">
             <input onChange={props.onInputChange} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
             <button type="submit" className="btn btn-primary btn-lg btn-block">Search new recipes</button>
         </form>
