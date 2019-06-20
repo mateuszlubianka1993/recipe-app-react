6e19430074fafeff22ae1889a17de143a8a87e4a
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-20 17:24:09 +0200

    add SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
@@ -7,6 +8,7 @@ class App extends React.Component {
     return(
       <div>
         <Header />
+        <SearchBar />
       </div>
     );
   };
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+
+    return(
+        <form className="container">
+                <input type="text" />
+                <button type="submit">Search</button>
+        </form>
+    );
+};
+
+export default SearchBar;
