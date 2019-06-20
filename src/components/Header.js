3b396a47c5c2b2de27508d7a419ef4338308fbce
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-20 17:19:16 +0200

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <h1 className="header-txt">Recipe App</h1>
+        </div>
+    );
+};
+
+export default Header;
