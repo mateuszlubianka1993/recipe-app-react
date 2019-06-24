5584966f11c4730a7bc79726f512ca2719016324
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-24 12:23:21 +0200

    add properties to Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,7 +3,36 @@ import React from 'react';
 const Recipe = ({recipe}) => {
 
     return(
-        <div>{recipe.recipe.label}</div>
+        <div className="card">
+            <img src={recipe.recipe.image} className="card-img-top" alt={recipe.recipe.label}/>
+            <div className="card-body">
+                <h5 className="card-title">{recipe.recipe.label}</h5>
+                <p className="card-text">Health labels: {recipe.recipe.healthLabels.map(label=><span className="badge badge-secondary" key={label}> {label}</span>)}</p>
+                <div className="description-container">
+                    <div className="description-box">
+                        <div className="alert alert-primary" role="alert">
+                            {recipe.recipe.calories.toFixed(2)} kcal
+                        </div>
+                        <div className="alert alert-primary" role="alert">
+                            Fat: {recipe.recipe.digest[0].total.toFixed(2)} g
+                        </div>
+                        <div className="alert alert-primary" role="alert">
+                            Carbs: {recipe.recipe.digest[1].total.toFixed(2)} g
+                        </div>
+                        <div className="alert alert-primary" role="alert">
+                            Protein: {recipe.recipe.digest[2].total.toFixed(2)} g
+                        </div>
+                        <div className="alert alert-primary" role="alert">
+                            Cholesterol: {recipe.recipe.digest[3].total.toFixed(2)} mg
+                        </div>
+                    </div>
+                    <div className="description-box">
+                        {recipe.recipe.ingredientLines.map(ingredient=><div className="alert alert-primary" key={ingredient} role="alert">{ingredient}</div>)}
+                    </div>
+                </div>
+                <a href={recipe.recipe.url} className="btn btn-primary">Get Recipe</a>
+            </div>
+        </div>
     );
 };
 
