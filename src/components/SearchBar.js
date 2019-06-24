70cd9b57eb11b94e8644cd57e7d883b3eea6139d
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-24 14:00:04 +0200

    add google fonts

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,5 @@
 import React from 'react';
-
-// const SearchBar = (props) => {
-
-//     return(
-//         <form onSubmit={props.onFormSubmit} className="container">
-//             <input onChange={props.onInputChange} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
-//             <button type="submit" className="btn btn-primary btn-lg btn-block">Search new recipes</button>
-//         </form>
-//     );
-// };
+import './styles/SearchBar.css';
 
 class SearchBar extends React.Component {
     constructor(props) {
@@ -21,7 +12,7 @@ class SearchBar extends React.Component {
             this.inputRef.current.value = '';
         };
         return(
-            <form onSubmit={this.props.onFormSubmit} className="container">
+            <form onSubmit={this.props.onFormSubmit} className="container search-container">
                 <input onChange={this.props.onInputChange} ref={this.inputRef} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
                 <button type="submit" onClick={clear} className="btn btn-primary btn-lg btn-block">Search new recipes</button>
             </form>
