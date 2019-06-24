270a6c0fefaaedd4257cf95ef9bd5d6e0f4338a5
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-24 13:46:23 +0200

    add clear function to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-const SearchBar = (props) => {
+// const SearchBar = (props) => {
 
-    return(
-        <form onSubmit={props.onFormSubmit} className="container">
-            <input onChange={props.onInputChange} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
-            <button type="submit" className="btn btn-primary btn-lg btn-block">Search new recipes</button>
-        </form>
-    );
+//     return(
+//         <form onSubmit={props.onFormSubmit} className="container">
+//             <input onChange={props.onInputChange} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+//             <button type="submit" className="btn btn-primary btn-lg btn-block">Search new recipes</button>
+//         </form>
+//     );
+// };
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.inputRef = React.createRef();
+    };
+
+    render() {
+        const clear = () => { 
+            this.inputRef.current.value = '';
+        };
+        return(
+            <form onSubmit={this.props.onFormSubmit} className="container">
+                <input onChange={this.props.onInputChange} ref={this.inputRef} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                <button type="submit" onClick={clear} className="btn btn-primary btn-lg btn-block">Search new recipes</button>
+            </form>
+        );
+    };
 };
 
 export default SearchBar;
