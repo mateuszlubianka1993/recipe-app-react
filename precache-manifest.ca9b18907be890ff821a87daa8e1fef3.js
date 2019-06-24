bcc865f62ec38906a94f399d88e53e901915e19b
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-06-24 17:04:06 +0200

    Updates

---
+++ precache-manifest.ca9b18907be890ff821a87daa8e1fef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd3d523aafadb8ef572fe95c6f86db8",
+    "url": "/recipe-app-react/index.html"
+  },
+  {
+    "revision": "6445c2217754760d4e99",
+    "url": "/recipe-app-react/static/css/main.a31ab210.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/recipe-app-react/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "6445c2217754760d4e99",
+    "url": "/recipe-app-react/static/js/main.b9835ded.chunk.js"
+  },
+  {
+    "revision": "8b356a5895740587488e",
+    "url": "/recipe-app-react/static/js/runtime~main.9f44707c.js"
+  },
+  {
+    "revision": "da86eda5b95a82d4d2823043203d2509",
+    "url": "/recipe-app-react/static/media/bread-color-copyspace-1565982.da86eda5.jpg"
+  }
+]);
